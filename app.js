e5e729012e654f524696e27ec6bfb752916b28e5
“FeryazHacimuradov” <tu201906125@code.edu.az>
2023-12-24 23:36:11 +0400

    something not work

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const mongoSanatize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
-const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -68,8 +67,6 @@ app.use(
     ],
   })
 );
-
-app.use(compression());
 
 // Routes
 
--- server.js
+++ server.js
@@ -18,7 +18,8 @@ const DB = process.env.DATABASE.replace(
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
   })
   .then(() => console.log('DB connection successful!'));
 
